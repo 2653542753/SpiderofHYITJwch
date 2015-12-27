a7210f23435dcc18d11462cccb37a7d62c22ed9b
Pan shuxin <psxjoy@outlook.com>
2015-12-27 19:48:42 +0800

    Arrange Function
    The part of the process is converted to a function

--- main.py
+++ main.py
@@ -27,26 +27,28 @@ def SendMessage(title):
     server.sendmail(from_addr, [to_addr], msg.as_string())
     server.quit()
 
-CheckNum = 1
-self = 2594
-main1Page = 'http://jwch.hyit.edu.cn/index.aspx?menuid=29&type=articleinfo&lanmuid=121&infoid='
-for k in range(1, 10000):
-    try:
-        main_html = requests.get(main1Page + str(self))
-        main_text = main_html.text
-        soup = BeautifulSoup(main_text, "html.parser")
-        final_title = soup.title.string
-        title = final_title[:-9]
-        self += 1
-        print(title + str(self))
-        SendMessage(title)
-        sleep(1)
-    except:
-        print('无法查询到，进行下一次查询' + str(self))
-        self += 1
-        CheckNum += 1
-        if (CheckNum > 3):
-            CheckNum = 0
-            break
-        else:
-            continue
+def analyiseWeb(WebNum):
+    mainpage = 'http://jwch.hyit.edu.cn/index.aspx?menuid=29&type=articleinfo&lanmuid=121&infoid='
+    CheckNum = 0
+    for i in range(1,10000):
+        try:
+            main_html = requests.get(mainpage + str(WebNum))
+            main_text = main_html.text
+            soup = BeautifulSoup(main_text, "html.parser")
+            final_title = soup.title.string
+            title = final_title[:-9]
+            WebNum += 1
+            print(title + str(WebNum))
+            #SendMessage(title)
+            sleep(1)
+        except:
+            print('无法查询到，进行下一次查询' + str(WebNum))
+            WebNum += 1
+            CheckNum += 1
+            if (CheckNum > 3):
+                CheckNum = 0
+                break
+            else:
+                continue
+
+print(analyiseWeb(2590))
