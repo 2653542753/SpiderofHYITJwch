97352439a066a3061769b03f4834d73329c5b0f8
Pan shuxin <psxjoy@outlook.com>
2015-12-27 19:39:04 +0800

    change
    fina;

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ def SendMessage(title):
         name, addr = parseaddr(s)
         return formataddr((Header(name, 'utf-8').encode(), addr))
 
-    from_addr = '***'
-    password = '***'
-    to_addr = '***'
-    smtp_server = '***'
+    from_addr = '****'
+    password = '****'
+    to_addr = '****'
+    smtp_server = 'smtp.163.com'
     msg = MIMEText(title, 'plain', 'utf-8')
-    msg['From'] = _format_addr('教务网爬虫 <%s>' % from_addr)
+    msg['From'] = _format_addr('爬虫提醒 <%s>' % from_addr)
     msg['To'] = _format_addr('管理员 <%s>' % to_addr)
     msg['Subject'] = Header('教务网新闻更新', 'utf-8').encode()
     server = smtplib.SMTP(smtp_server, 25)
@@ -27,10 +27,10 @@ def SendMessage(title):
     server.sendmail(from_addr, [to_addr], msg.as_string())
     server.quit()
 
-
-self = 2568
+CheckNum = 1
+self = 2594
 main1Page = 'http://jwch.hyit.edu.cn/index.aspx?menuid=29&type=articleinfo&lanmuid=121&infoid='
-for k in range(1,10000):
+for k in range(1, 10000):
     try:
         main_html = requests.get(main1Page + str(self))
         main_text = main_html.text
@@ -38,7 +38,15 @@ for k in range(1,10000):
         final_title = soup.title.string
         title = final_title[:-9]
         self += 1
+        print(title + str(self))
         SendMessage(title)
-        sleep(900)
+        sleep(1)
     except:
-        break
+        print('无法查询到，进行下一次查询' + str(self))
+        self += 1
+        CheckNum += 1
+        if (CheckNum > 3):
+            CheckNum = 0
+            break
+        else:
+            continue
